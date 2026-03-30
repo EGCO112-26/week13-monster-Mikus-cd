a0ebfeebc4d1aad84679332ec62ca7f5fb6a45bb
Kim_Natthanon <kimachachan@gmail.com>
2026-03-30 08:35:25 +0000

    update

--- main.cpp
+++ main.cpp
@@ -10,6 +10,15 @@ using namespace std;
 #include "monster.h"
 
 int main(int argc, const char * argv[]) {
- 
-    return 0;
-}
+    monster *p;
+
+
+    monster m1("Naruto Uzumaki",10,1);
+    monster m2("lelouch"),m4("Cid Kagenou",30);
+    monster m3;
+    monster x[5]={{"Anos voldigoad",5},{"Monkey D Luffy",10,20}};
+    p=new monster("Lulu",10,50);
+    delete p;//destructor
+
+    return 0;//destructor x,m4,m2,m1
+}    
--- monster.h
+++ monster.h
@@ -10,12 +10,24 @@
 class monster{
 private:
     string name;
-    int hp,potion;
+    unsigned int hp,potion;
 public:
     void Attack(monster &);
           void heal();
-    monster(string, int,int);
+    monster(string="LnwZaa007 Kung", int=10,int=1);
+    ~monster();
 };
+
+monster::~monster(){
+    cout<<"Bye Bye "<<name<< " Hp: "<<hp<<endl;
+}
+monster::monster(string n,int h,int p){
+    name=n;
+    h>200?hp=200:hp=h;
+    p>10?potion=10:potion=p;
+
+    cout<<"Monster "<<name << " How should I feel "<<endl;
+}
 
 
 #endif /* monster_h */
